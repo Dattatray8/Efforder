7f5684e658799ae7a598814d01fcc8339ff4f993
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-07-30 12:57:37 +0530

    Add Cloudinary and Multer configuration for file uploads

---
+++ server/config/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const uploadCloudinary = async (filePath) => {
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUNDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUNDINARY_API_KEY,
+      api_secret: process.env.CLOUNDINARY_API_SECRET,
+    });
+    if (!filePath) {
+      return null;
+    }
+    const upload = await cloudinary.uploader.upload(filePath);
+    fs.unlinkSync(filePath);
+    return upload.secure_url;
+  } catch (error) {
+    fs.unlinkSync(filePath);
+    console.log("Error uploading to Cloudinary:", error);
+  }
+};
+
+export default uploadCloudinary;
