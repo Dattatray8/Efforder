14ea088c9b2fe253582372e54fc89763c68d9946
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-12-06 18:47:49 +0530

    chore: update .gitignore to include .env file, refactor AuthContext to use environment variable for server URL, and add .gitkeep to server public directory

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = import.meta.env.VITE_SERVER_URL;
   const value = {
     serverUrl,
   };
